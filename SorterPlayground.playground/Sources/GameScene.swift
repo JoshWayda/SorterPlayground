286d7b9313aef9b836022757a1b2475c683454ab
Josh Wayda <josh.wayda@gmail.com>
2019-01-22 01:36:15 -0600

    Update GameScene.swift
    Update the color pallette of the squares to make them more pleasing to look at.
    Added selection sort implementation

--- SorterPlayground.playground/Sources/GameScene.swift
+++ SorterPlayground.playground/Sources/GameScene.swift
@@ -23,20 +23,25 @@ public class GameScene: SKScene {
     
     public override func didMove(to view: SKView) {
         let sorter = SortDirector(scene:self)
-        sorter.addSortNode(color: UIColor.blue, value: "3")
-        sorter.addSortNode(color: UIColor.gray, value: "9")
-        sorter.addSortNode(color: UIColor.orange, value: "6")
-        sorter.addSortNode(color: UIColor.darkGray, value: "7")
+        //9 2 1 8 4 5 7 6 3
+        sorter.addSortNode(color: UIColor(rgb: 0x967ADC), value: "9")
+        sorter.addSortNode(color: UIColor(rgb: 0xE9573F), value: "2")
         
-        sorter.addSortNode(color: UIColor.cyan, value: "8")
-        sorter.addSortNode(color: UIColor.magenta, value: "5")
-        sorter.addSortNode(color: UIColor.purple, value: "4")
         
-        sorter.addSortNode(color: UIColor.red, value: "1")
-        sorter.addSortNode(color: UIColor.green, value: "2")
+        sorter.addSortNode(color: UIColor(rgb: 0xA0D468), value: "4")
+        sorter.addSortNode(color: UIColor(rgb: 0x4A89DC), value: "7")
+        sorter.addSortNode(color: UIColor(rgb: 0x4BCFAD), value: "5")
+        
+        
+        sorter.addSortNode(color: UIColor(rgb: 0x5D9CEC), value: "6")
+        sorter.addSortNode(color: UIColor(rgb: 0xED5565), value: "1")
+        sorter.addSortNode(color: UIColor(rgb: 0xFFCE54), value: "3")
+        sorter.addSortNode(color: UIColor(rgb: 0xAC92EC), value: "8")
         sorter.ready()
         
-        self.insertionSort(director: sorter)
+        self.selectionSort(director: sorter)
+        //sorter.swap(leftIndex: 2, rightIndex: 7)
+        //self.insertionSort(director: sorter)
     }
  
     func insertionSort(director:SortDirector){
@@ -75,6 +80,64 @@ public class GameScene: SKScene {
         }
     }
 
+    //SELECTION SORT IMPLEMENTATION
+    /*
+     /* a[0] to a[n-1] is the array to sort */
+     int i,j;
+     int n; // initialise to a's length
+     
+     /* advance the position through the entire array */
+     /*   (could do j < n-1 because single element is also min element) */
+     for (j = 0; j < n-1; j++)
+     {
+     /* find the min element in the unsorted a[j .. n-1] */
+     
+     /* assume the min is the first element */
+     int iMin = j;
+     /* test against elements after j to find the smallest */
+     for (i = j+1; i < n; i++)
+     {
+     /* if this element is less, then it is the new minimum */
+     if (a[i] < a[iMin])
+     {
+     /* found new minimum; remember its index */
+     iMin = i;
+     }
+     }
+     
+     if (iMin != j)
+     {
+     swap(a[j], a[iMin]);
+     }
+     }
+     */
+
+    func selectionSort(director:SortDirector) {
+        var A = director.sortNodeList
+        let n : Int = A.count
+        printlist(list: A)
+        for j in 0 ..< n-1 {
+            var iMin : Int = j
+            for i in j+1 ..< n {
+                if A[i].value < A[iMin].value {
+                    iMin = i
+                }
+            }
+            
+            if iMin != j {
+                printlist(list:director.sortNodeList)
+                print("swap: A[\(j)]:\(A[j].value) with A[\(iMin)]:\(A[iMin].value) : move \(iMin - j)")
+                //director.swap(leftIndex: j, rightIndex: iMin)
+                director.swap(leftIndex: director.getIndex(fromValue: A[j].value), rightIndex: director.getIndex(fromValue: A[iMin].value), distance: iMin - j)
+                let temp = A[j]
+                A[j] = A[iMin]
+                A[iMin] = temp
+                printlist(list: A)
+            }
+        }
+    }
+    
+    
     func printlist(list:[SortNode]) {
         var l : String = ""
         for n in list {
